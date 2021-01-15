27a793802a81cd7ecfa0f850a3ed8c940dad6082
d3v53c <shitfi63@gmail.com>
2021-01-15 05:26:53 -0800

    prototype pollution fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+const isPrototypePolluted = function(key: string) {
+  return ['__proto__', 'prototype', 'constructor'].indexOf(key) > -1;
+}
+
 export default function set(obj: Record<string, any> = {}, path: string, value: any) {
   const [key, ...keys] = path.split('.');
+  if (isPrototypePolluted(key)) return obj;
   const pointer = obj[key] || {};
   obj[key] =
     keys.length < 1
